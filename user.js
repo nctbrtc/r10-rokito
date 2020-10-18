ae04230fffd38875d11a7538062ae081b1417769
nctbrtc <41221911+nctbrtc@users.noreply.github.com>
2020-10-18 01:14:21 -0400

    Update user.js

--- user.js
+++ user.js
@@ -1,2 +1,3 @@
+document.querySelector("#merve > img").src="https://cdn.r10.net/bildirim.png";
 document.querySelector("#merve > img").style.height="30px";
 document.querySelector("#merve > img").style.width="30px";
